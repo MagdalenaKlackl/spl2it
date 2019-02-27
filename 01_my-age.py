eeaf477c7cb1543f91ca28969e71d698f6e41574
Magdalena Klackl <171wita13@o365.htl-leoben.at>
2019-02-27 14:36:02 +0100

    created hello 01_my-age.py

---
+++ 01_my-age.py
@@ -0,0 +1,8 @@
+input("What is your name?")
+name = input("What is your name?")
+input("How old are you?")
+alter = input("How old are you?")
+alter = int(alter)
+
+nextYear = alter + 1
+
--- helloworld1.py
+++ helloworld1.py
@@ -1,1 +1,4 @@
 print ("hello world!")
+
+print ("hello", "World", "Internet", "School")
+print ("hello"+ "World"+ "Internet"+ "School")
