44a3cc72ff922e374ec612adaae8032a064bea56
Magdalena Klackl <171wita13@o365.htl-leoben.at>
2019-03-06 13:07:58 +0100

    created printing.py

---
+++ for-schleifen.py
@@ -0,0 +1,17 @@
+for i in 1,2,3,4,5:
+		print (i)
+		
+for i in -4,-3,-2,-1,0,1,2,3,4;
+		print(i)
+		
+for i in "Alex", "Klaus", 22, 45, "Peter", 69;
+		print(i)
+		
+for i in range(0,9):
+	print(i)
+	
+for i in range(1,17,4):
+	print(i)
+	
+for i  in range(-5, 5, 2):
+	print(i)
---
+++ printing.py
@@ -0,0 +1,12 @@
+namen = ["Klaus", "Peter", "Markus", "Gabi", "Susi", "Elias"]
+	print(namen)
+	
+for name in namen:
+	print(name, end=",")
+	
+print()
+
+print("Hallo"."mein","Name","ist","Peter")
+print("Hallo"."mein","Name","ist","Peter", = sep="...")
+
+print("-" *20)
